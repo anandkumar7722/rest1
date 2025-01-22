295f069037896f870bb5b2555b235aa09d190a79
Anand777 <ffanand429@gmail.com>
2025-01-22 12:56:38 +0000

    22

--- text.php
+++ text.php
@@ -99,3 +99,113 @@ print("master"."<br>");
 echo 99**66 ."<br>";
 echo 98*2;
 ?>
+<?php
+print("20/1/25"."is was delete!!");
+print("it redoing ");
+//arrays
+print("22/1/25");
+print("reback");
+$arr=[1,2,3,4,5];
+//    0 1 2 3 4
+echo "<br>".$arr[0];
+echo "<br>".$arr[1];
+echo "<br>".$arr[2];
+echo "<br>".$arr[3];
+echo "<br>".$arr[4];
+//reverse acces elements
+/*
+echo "<br>".$arr[-1];
+echo "<br>".$arr[-2];
+echo "<br>".$arr[-3];
+echo "<br>".$arr[-4];
+*/
+echo"<hr>";
+for ($i=0;$i<4;$i++){
+    printf("<br>"."%d",$arr[$i]);
+}
+//asscoiative arrays
+$arr1=[1,3.4,true,"hello",$arr[0],012,0xff];
+print_r($arr1);
+//gettype
+for ($i=0;$i<8;$i++){
+    printf("<br>".gettype($arr1[$i]));
+}
+//count
+for ($i=0;$i<=count($arr1);$i++){
+    printf("<br>"."%d",$arr[$i]);
+}
+print("<br>".array_rand($arr));
+print("<br>".array_chunk($arr,1));
+$arr2=[
+    "one"=>1,
+    "one0"=>2,
+    "one1"=>3,
+    "one3"=>0b01,
+    "one4"=>0xff,
+    "one4"=>"5.5",
+    "one6"=>"keyss"
+];
+print($arr2["one"]);
+print($arr2["one0"]);
+print($arr2["one1"]);
+//replace function
+print_r($arr2);
+$filps=print_r(array_flip($arr2));
+echo"<hr>";
+function testfunc($key,$value){
+    print("$key:$value<br>");
+}
+print_r(array_walk($arr2,'testfunc'));
+print_r(array_key_exists('hello',$arr2));
+
+if(array_key_exists('one1',$arr2)){
+    print("<br>"."is exist");
+}
+else{
+    print("is  not exist");
+}
+if(array_key_exists('one0',$arr2)){
+    print("<br>"."is exist");
+}
+else{
+    print("is  not exist"."<br>");
+}
+?><pre><?
+print_r($arr2);
+print_r(array_chunk($arr2,3,true));
+print_r(array_chunk($arr2,3));
+print_r(array_chunk($arr2,3));
+$arre=[11,23,44,55,66,7,7];
+print_r(array_pad($arre,13,87));
+print_r(array_pad($arre,9,87));
+print_r(array_pad($arre,9,"hello"));
+$arrp=[
+    'apple'=>[
+        'red',
+        'green'
+    ],
+    'organ'=>[
+        'fruit',
+        'green organ'
+    ]
+    ];
+print_r(array_replace_recursive($arrp,[
+    'apple'=>[
+        'good for healt'
+    ]
+]));
+print_r(array_replace_recursive($arrp,[
+    'organ'=>[
+        'good for healt',
+        'is the organ'
+    ]
+]));
+print_r(array_sum($arr));
+print_r($arr);
+print_r(array_pop($arr));
+print_r(array_push($arr,5,5,7,7,9,4,5,8,9,4));
+print_r($arr);
+print_r(array_unique($arr));
+print_r(array_push($arr,5,5,7,7,9,4,5,8,9,4.435,446,754665));
+print_r(array_unique($arr));
+?></pre>
