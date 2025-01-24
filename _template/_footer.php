3ebf15b1dc85684d5f06f6ec3d751eb2d5d32a50
Anand777 <ffanand429@gmail.com>
2025-01-24 15:17:18 +0000

    24/1/25

---
+++ _template/_footer.php
@@ -0,0 +1,11 @@
+<footer style="background-color: #222; color: white; text-align: center; padding: 20px;">
+    <div>
+        <a href="https://facebook.com" target="_blank"
+            style="color: white; margin: 0 10px; text-decoration: none;">Facebook</a>
+        <a href="https://twitter.com" target="_blank"
+            style="color: white; margin: 0 10px; text-decoration: none;">Twitter</a>
+        <a href="https://linkedin.com" target="_blank"
+            style="color: white; margin: 0 10px; text-decoration: none;">LinkedIn</a>
+    </div>
+    <p>&copy; 2025 YourCompany. All Rights Reserved.</p>
+</footer>
---
+++ libs/load.php
@@ -0,0 +1,8 @@
+<?php
+
+function load_template($name){
+    //print(__FILE__);
+    include __DIR__."/../_template/$name.php";
+    
+}
+?>
