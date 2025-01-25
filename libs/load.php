c33bd4634e52e42976633a59f284362dd324e29b
Anand777 <ffanand429@gmail.com>
2025-01-25 13:43:57 +0000

    25/1/25_e

--- libs/load.php
+++ libs/load.php
@@ -2,7 +2,8 @@
 
 function load_template($name){
     //print(__FILE__);
-    include __DIR__."/../_template/$name.php";
+    //include __DIR__."/../_template/$name.php";
+    include $_SERVER['DOCUMENT_ROOT']."/app/_template/$name.php";
     
 }
 ?>
--- test.php
+++ test.php
@@ -1,5 +1,19 @@
 <pre>
     <?php
+    $cookie_name = "user";
+    $cookie_value = $_SERVER['REQUEST_URI'];
+    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+    print("_SERVER \n");
     print_r($_SERVER);
+    print("_GET \n");
+    print_r($_GET);
+    print("_POST \n");
+    print_r($_POST);
+    print("file");
+    print_r($_FILES);
+    print("cookie");
+    print_r($_COOKIE);
+    //print("session");
+    //print_r($_SESSION);
     ?>
 </pre>
